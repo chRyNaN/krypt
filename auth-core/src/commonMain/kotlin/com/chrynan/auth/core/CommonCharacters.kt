bca3ec7bdf43d8de27269e6e51b283bc494c69c0
Christopher Keenan <ckeenan@starry.com>
2021-11-30 17:34:06 -0600

    Cleaned up auth-core module

--- auth-core/src/commonMain/kotlin/com/chrynan/auth/core/CommonCharacters.kt
+++
@@ -1,4 +0,0 @@
-package com.chrynan.auth.core
-
-private const val ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-val ALPHA_NUMERIC_CHARS = ALPHA_NUMERIC_STRING.toCharArray()
