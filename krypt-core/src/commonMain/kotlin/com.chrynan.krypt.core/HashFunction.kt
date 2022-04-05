01fde398a86ea5764fc36db2e32080c9eaaadf18
Christopher Keenan <ckeenan@starry.com>
2022-04-05 09:28:23 -0500

    Renamed HashFunction Result generic parameter to Output to match Input generic parameter

--- krypt-core/src/commonMain/kotlin/com.chrynan.krypt.core/HashFunction.kt
+++ krypt-core/src/commonMain/kotlin/com.chrynan.krypt.core/HashFunction.kt
@@ -4,7 +4,7 @@ package com.chrynan.krypt.core
 
 /**
  * Represents a cryptographic hash function, or a secure one-way hash function that takes arbitrarily sized input data
- * of type [Input] and outputs a value of type [Result], which contains or is a fixed size hash value
+ * of type [Input] and outputs a value of type [Output], which contains or is a fixed size hash value
  * ("message-digest") as a result of this function. The result of performing a hash function cannot be reversed,
  * meaning that the input value cannot be obtained by reversing the hash, as a hash function is a one-way function.
  * Different implementations of this interface may represent different hashing algorithms, such as "SHA256" or
@@ -13,12 +13,12 @@ package com.chrynan.krypt.core
  * @see [Hash Function](https://en.wikipedia.org/wiki/Cryptographic_hash_function)
  * @see [Key Derivation Function](https://en.wikipedia.org/wiki/Key_derivation_function)
  */
-interface HashFunction<Input, Result> {
+interface HashFunction<Input, Output> {
 
     /**
-     * Performs the hash with the provided [source] value and returns a [Result].
+     * Performs the hash with the provided [source] value and returns a [Output].
      */
-    suspend operator fun invoke(source: Input): Result
+    suspend operator fun invoke(source: Input): Output
 
     companion object
 }
@@ -27,4 +27,4 @@ interface HashFunction<Input, Result> {
  * A convenience function that delegates to the [invoke] function of the [HashFunction] interface. This is provided for
  * preference, as it may be preferable to use "hash" instead of "invoke" at the call-site.
  */
-suspend fun <Input, Result> HashFunction<Input, Result>.hash(source: Input): Result = invoke(source = source)
+suspend fun <Input, Output> HashFunction<Input, Output>.hash(source: Input): Output = invoke(source = source)
