412b27640786d36d2e077ad2a9a1482e5edd3353
Christopher Keenan <ckeenan@starry.com>
2022-04-12 10:35:55 -0500

    Removed krypt-hash-sha module

--- krypt-hash-sha/src/commonMain/kotlin/com.chrynan.krypt.hash.sha/SHAHashResult.kt
+++
@@ -1,9 +0,0 @@
-package com.chrynan.krypt.hash.sha
-
-import com.chrynan.krypt.core.SecureString
-import com.chrynan.krypt.hash.HashResult
-
-data class SHAHashResult internal constructor(
-    override val algorithm: SHAHashAlgorithm,
-    override val hash: SecureString
-) : HashResult<SHAHashAlgorithm, SecureString>
