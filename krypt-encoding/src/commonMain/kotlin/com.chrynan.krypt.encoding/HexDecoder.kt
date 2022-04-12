d3ba902a8c3639d0c9b840c7bb2e6651f6a49189
Christopher Keenan <ckeenan@starry.com>
2022-04-11 22:21:48 -0500

    Created HexDecoder and HexEncoder

---
+++ krypt-encoding/src/commonMain/kotlin/com.chrynan.krypt.encoding/HexDecoder.kt
@@ -0,0 +1,29 @@
+@file:Suppress("unused")
+
+package com.chrynan.krypt.encoding
+
+/**
+ * A [Decoder] that decodes a hexadecimal formatted [String] value into a [ByteArray]. Use the [decodeToByteArray]
+ * function for proper decoding.
+ */
+class HexDecoder : Decoder {
+
+    override fun decodeToByteArray(source: ByteArray): ByteArray =
+        // FIXME: not sure if this works. Might lose value when encoding from String to ByteArray and back to String for hexadecimal String.
+        source.decodeToString()
+            .chunked(2)
+            .map { it.toInt(16).toByte() }
+            .toByteArray()
+
+    override fun decodeToString(source: ByteArray): String =
+        throw UnsupportedOperationException("A hexadecimal ByteArray cannot be decoded into a String. Use the decodeToByteArray function instead.\nBytes are not equivalent to hexadecimal values. An encoded hexadecimal value would be a String.")
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun decodeToByteArray(source: String): ByteArray {
+        require(source.length % 2 == 0) { "Invalid hexadecimal String. Cannot convert to a ByteArray." }
+
+        return source.chunked(2)
+            .map { it.toInt(16).toByte() }
+            .toByteArray()
+    }
+}
---
+++ krypt-encoding/src/commonMain/kotlin/com.chrynan.krypt.encoding/HexEncoder.kt
@@ -0,0 +1,40 @@
+@file:Suppress("unused")
+
+package com.chrynan.krypt.encoding
+
+/**
+ * An [Encoder] that encodes a [ByteArray] into a hexadecimal [String] value.
+ *
+ * @property [lowercase] Whether the output of the [encodeToString] function should encode the characters to lowercase
+ * values or not. The default is `false`. A value of `true`, will result in lowercase character values. A value of
+ * `false` will result in uppercase character values.
+ */
+class HexEncoder(
+    private val lowercase: Boolean = false
+) : Encoder {
+
+    override fun encodeToByteArray(source: ByteArray): ByteArray =
+        throw UnsupportedOperationException("A ByteArray cannot be encoded into a hexadecimal ByteArray. Use the encodeToString function instead.\nBytes are not equivalent to hexadecimal values. An encoded hexadecimal value would be a String.")
+
+    override fun encodeToString(source: ByteArray): String {
+        val hexChars = CharArray(source.size * 2)
+
+        for (i in source.indices) {
+            val v = source[i].toInt() and 0xFF
+
+            hexChars[i * 2] = HEX_ARRAY[v ushr 4]
+            hexChars[i * 2 + 1] = HEX_ARRAY[v and 0x0F]
+        }
+
+        return if (lowercase) {
+            hexChars.concatToString().lowercase()
+        } else {
+            hexChars.concatToString()
+        }
+    }
+
+    companion object {
+
+        private val HEX_ARRAY = "0123456789ABCDEF".toCharArray()
+    }
+}
