45270606e45cddbb676d4ebca5dfda69ee7450e2
Christopher Keenan <ckeenan@starry.com>
2022-06-11 12:37:18 -0500

    Implemented PBKDF2HashFunction

---
+++ krypt-hash/src/commonMain/kotlin/com.chrynan.krypt.hash/pbkdf/PBKDF2Algorithm.kt
@@ -0,0 +1,39 @@
+package com.chrynan.krypt.hash.pbkdf
+
+import com.chrynan.krypt.hash.HashAlgorithm
+
+internal data class PBKDF2Algorithm(
+    val salt: ByteArray,
+    val iterationCount: Int,
+    val hLength: Int,
+    val dkLength: Int
+) : HashAlgorithm {
+
+    override val name: String
+        get() = "PBKDF2"
+
+    override val version: String
+        get() = "2"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as PBKDF2Algorithm
+
+        if (!salt.contentEquals(other.salt)) return false
+        if (iterationCount != other.iterationCount) return false
+        if (name != other.name) return false
+        if (version != other.version) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = salt.contentHashCode()
+        result = 31 * result + iterationCount
+        result = 31 * result + name.hashCode()
+        result = 31 * result + version.hashCode()
+        return result
+    }
+}
--- krypt-hash/src/commonMain/kotlin/com.chrynan.krypt.hash/pbkdf/PBKDF2HashFunction.kt
+++ krypt-hash/src/commonMain/kotlin/com.chrynan.krypt.hash/pbkdf/PBKDF2HashFunction.kt
@@ -1,13 +1,81 @@
 package com.chrynan.krypt.hash.pbkdf
 
+import com.chrynan.krypt.core.Endian
 import com.chrynan.krypt.core.HashFunction
+import com.chrynan.krypt.core.toByteArray
+import com.chrynan.krypt.core.toInt
 
-internal class PBKDF2HashFunction : HashFunction<ByteArray, ByteArray> {
+/**
+ * An implementation of the PBKDF2 (password-based key derivation function 2) key derivation function. A key derivation
+ * function is a hash function that derives an output key from an input key. In the case of password-based key
+ * derivation functions, the input key would be the password. The PBKDF2 key derivation function can be used as a
+ * password hash function or a means to derive a new key value.
+ *
+ * The PBKDF2 takes in a password, a salt, and an iteration count as parameters. It performs a psuedorandom function to
+ * the input password and salt value and repeats the process for the iteration count amount of times. This produces the
+ * output derived key value. The added computational work of performing the operations an iteration count amount of
+ * times, makes password cracking computationally expensive and difficult. This process is known as "key stretching".
+ *
+ * The PBKDF2 function is defined as the following algorithm:
+ *
+ * ```
+ * DK = PBKDF2(PRF, Password, Salt, c, dkLen)
+ * ```
+ *
+ * @see [RFC Specification](https://datatracker.ietf.org/doc/html/rfc2898)
+ * @see [Wikipedia Explanation](https://en.wikipedia.org/wiki/PBKDF2)
+ */
+internal class PBKDF2HashFunction(
+    private val hashFunction: HashFunction<ByteArray, ByteArray>,
+    private val algorithm: PBKDF2Algorithm
+) : HashFunction<ByteArray, ByteArray> {
 
-    override val algorithmName: String?
-        get() = TODO("Not yet implemented")
+    override val algorithmName: String
+        get() = algorithm.name
 
     override suspend fun invoke(source: ByteArray): ByteArray {
-        TODO("Not yet implemented")
+        var result = byteArrayOf()
+
+        for (i in 1..algorithm.dkLength / algorithm.hLength) {
+            result += f(password = source, salt = algorithm.salt, c = algorithm.iterationCount, i = i)
+        }
+
+        return result
+    }
+
+    /**
+     * Performs the F function defined in the specification.
+     *
+     * ```
+     * Ti = F(Password, Salt, c, i)
+     * F(Password, Salt, c, i) = U1 ^ U2 ^ ⋯ ^ Uc
+     *
+     * where:
+     * U1 = PRF(Password, Salt + INT_32_BE(i))
+     * U2 = PRF(Password, U1)
+     * ⋮
+     * Uc = PRF(Password, Uc−1)
+     * ```
+     *
+     * @see [RFC Specification](https://datatracker.ietf.org/doc/html/rfc2898#section-5.2)
+     * @see [Wikipedia Definition](https://en.wikipedia.org/wiki/PBKDF2#Key_derivation_process)
+     */
+    private suspend fun f(password: ByteArray, salt: ByteArray, c: Int, i: Int): ByteArray {
+        // `i` (the block index value), is converted to a ByteArray in big-endian format. This is used for the first
+        // iteration of this function's computation. Note that `i` should be a one-based index.
+        val bigEndianI = i.toByteArray(order = Endian.Big)
+
+        // Compute the first iteration to derive `U1`
+        // `U1 = PRF(Password, Salt + INT_32_BE(i))`
+        var previousU = hashFunction(source = password + salt + bigEndianI)
+        var result = previousU.toInt()
+
+        (0 until c).forEach { _ ->
+            previousU = hashFunction(source = password + previousU)
+
+            result = result xor previousU.toInt()
+        }
+
+        return result.toByteArray()
     }
 }
