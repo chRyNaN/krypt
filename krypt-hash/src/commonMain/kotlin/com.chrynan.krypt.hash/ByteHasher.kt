c83dcf3d3122eaa03dae44d069fba8f7d251e548
Christopher Keenan <ckeenan@starry.com>
2022-06-11 13:20:29 -0500

    Added overloaded convenience functions to ByteHasher

--- krypt-hash/src/commonMain/kotlin/com.chrynan.krypt.hash/ByteHasher.kt
+++ krypt-hash/src/commonMain/kotlin/com.chrynan.krypt.hash/ByteHasher.kt
@@ -1,5 +1,7 @@
 package com.chrynan.krypt.hash
 
+import com.chrynan.krypt.core.hash
+
 /**
  * A specialized and commonly used [Hasher] that operates on an input type of [ByteArray] and outputs a [ByteArray] in
  * the result.
@@ -9,3 +11,21 @@ interface ByteHasher<Algorithm : HashAlgorithm, Result : HashResult<Algorithm, B
 
     companion object
 }
+
+/**
+ * A convenience function that calls the [ByteHasher.invoke] function with the provided [source] [String] value encoded
+ * to a [ByteArray] using the [encodeToByteArray] function. This encodes the provided [source] [String] as a UTF-8
+ * [ByteArray].
+ */
+suspend fun <Algorithm : HashAlgorithm, Result : HashResult<Algorithm, ByteArray>> ByteHasher<Algorithm, Result>.invoke(
+    source: String
+): Result = invoke(source = source.encodeToByteArray())
+
+/**
+ * A convenience function that calls the [ByteHasher.hash] function with the provided [source] [String] value encoded
+ * to a [ByteArray] using the [encodeToByteArray] function. This encodes the provided [source] [String] as a UTF-8
+ * [ByteArray].
+ */
+suspend fun <Algorithm : HashAlgorithm, Result : HashResult<Algorithm, ByteArray>> ByteHasher<Algorithm, Result>.hash(
+    source: String
+): Result = hash(source = source.encodeToByteArray())
